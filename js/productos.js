cf4fd794390dd4b9ee8364fdec16e801110fb318
juan.ignaciobustos7@gmail.com <juan.ignaciobustos7@gmail.com>
2023-01-21 01:58:03 -0300

    correccion de rutas

--- js/productos.js
+++ js/productos.js
@@ -1,12 +1,11 @@
 let productos = [];
 
-fetch("./json/productos.json")
+fetch("./js/productos.json")
     .then(response => response.json())
     .then(data => {
         productos = data;
         cargarProductos(productos);
     })
-
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
@@ -22,16 +21,16 @@ function cargarProductos(productosElegidos) {
 
     contenedorProductos.innerHTML = "";
 
-    productosElegidos.forEach(producto => {
+    productosElegidos.forEach(productos => {
 
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <img class="producto-imagen" src="${productos.imagen}" alt="${productos.titulo}">
             <div class="producto-detalles">
-                <h3 class="producto-titulo">${producto.titulo}</h3>
-                <p class="producto-precio">$${producto.precio}</p>
-                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+                <h3 class="producto-titulo">${productos.titulo}</h3>
+                <p class="producto-precio">$${productos.precio}</p>
+                <button class="producto-agregar" id="${productos.id}">Agregar</button>
             </div>
         `;
 
@@ -117,3 +116,33 @@ function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
 }
+
+const navbarindex = document.getElementById("navbarindex");
+
+navbarindex.innerHTML = `    <div id="navbarindex">
+
+<nav class="navbar navbar-expand-lg bg-lav">
+    <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./paginas/productos.html">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Carrito</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Contacto</a>
+            </li>
+            </ul>
+    </div>
+    </div>
+</nav>
+</div>
+`
